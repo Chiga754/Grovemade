82707b865f0057d0c9fddf947b7340c2725ca73c
Chiga754 <a.chigarev96@gmail.com>
2021-11-09 12:21:39 +0500

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    // Slider for Featured Products
+    $('.featured-products__items').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+    });
+})
