19ef0814ab8a461959d8b7aaf64bfd16b450e02d
Chiga754 <a.chigarev96@gmail.com>
2021-11-12 08:45:01 +0500

    Adaptive layout

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,36 @@
 $(function () {
+
+    // Header-menu
+    $('.header__btn').on('click', function () {
+        $('.header__menu').toggleClass('header__menu-show');
+    })
+
+
     // Slider for Featured Products
     $('.featured-products__items').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false,
         dots: true,
+        responsive: [
+            {
+                breakpoint: 970,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        ]
     });
-
-    // Hover to link 
-
 })
